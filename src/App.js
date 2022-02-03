aa026f92b02f047492a5612fe2a33caca76954cc
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-02-03 13:52:36 +0200

    does refactoring code

--- src/App.js
+++ src/App.js
@@ -1,63 +1,64 @@
-import { useState } from "react"
-import FeedbackButtons from "./FeedbackButtons/FeedbackButton"
-import Section from "./Section/Section"
-import Statistics from "./Statistics/Statistics"
-import Notification from "./Notification/Notification"
-import s from './App.module.css'
+import { useState } from 'react';
+import FeedbackButtons from './FeedbackButtons/FeedbackButton';
+import Section from './Section/Section';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+import s from './App.module.css';
 
 const App = () => {
-    const [good, setGood] = useState(0);
-    const [neutral, setNeutral] = useState(0);
-    const [bad, setBad] = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-    const handleClick = (e) => {
-        const { name } = e.target
-        switch (name) {
-            case 'good':
-                setGood(prevState => prevState + 1)
-                break;
-            case 'neutral':
-                setNeutral(prevState => prevState + 1)
-                break;
-            case 'bad':
-                setBad(prevState => prevState + 1)
-                break;
-            default:
-                return;
-        };
-    };
+  const handleClick = e => {
+    const { name } = e.target;
+    switch (name) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
+  };
 
-    const countTotalFeedback = () => {
-        return good + neutral + bad;
-    };
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
 
-    const countPositiveFeedbackPercentage = () => {
-        if (!good) {
-            return 0;
-        }
-        return (good * 100 / countTotalFeedback()).toFixed(0)
-    };
-    
-    return (
-        <section className={s.feedbackSection}>
-            <Section title={'Please leave feedback!'}>
-                <FeedbackButtons
-                    options={['good', 'neutral', 'bad']}
-                    onHandleClick={handleClick}
-                />
-            </Section>
-            {countTotalFeedback() === 0
-                ? <Notification message={"No feedback given"} />
-                : <Section title={'Statistics'}>
-                    <Statistics
-                        options={[`Good: ${good} `, `Neutral: ${neutral}`, `Bad: ${bad}`]}
-                        onCountTotalFeedback={countTotalFeedback()}
-                        onCountPositiveFeedbackPercentage={countPositiveFeedbackPercentage()}
-                    />
-                </Section>}
-        </section>
-    );
+  const countPositiveFeedbackPercentage = () => {
+    if (!good) {
+      return 0;
+    }
+    return ((good * 100) / countTotalFeedback()).toFixed(0);
+  };
+
+  return (
+    <section className={s.feedbackSection}>
+      <Section title="Please leave feedback!">
+        <FeedbackButtons
+          options={['good', 'neutral', 'bad']}
+          onHandleClick={handleClick}
+        />
+      </Section>
+      {countTotalFeedback() === 0 ? (
+        <Notification message="No feedback given" />
+      ) : (
+        <Section title="Statistics">
+          <Statistics
+            options={{ good, neutral, bad }}
+            onCountTotalFeedback={countTotalFeedback()}
+            onCountPositiveFeedbackPercentage={countPositiveFeedbackPercentage()}
+          />
+        </Section>
+      )}
+    </section>
+  );
 };
 
-
-export default App
+export default App;
--- src/Statistics/Statistics.js
+++ src/Statistics/Statistics.js
@@ -1,19 +1,33 @@
-import s from './Statistics.module.css'
+import s from './Statistics.module.css';
 
-const Statistics = ({options, onCountTotalFeedback, onCountPositiveFeedbackPercentage}) => (
-    <ul className={s.statList}>
-        {options.map(option => (
-            <li className={s.statBox} key={option}>
-                <p className={s.stat}>{option}</p>
-         </li>
-        ))}
-        <li className={s.statBox}>
-            <p className={s.total}>Total: {onCountTotalFeedback}</p>
-        </li>
-        <li className={s.statBox}>
-            <p className={s.positivePersentage}>Positive feedback: {onCountPositiveFeedbackPercentage}%</p>
-        </li>
-    </ul>
-)
+const Statistics = ({
+  options,
+  onCountTotalFeedback,
+  onCountPositiveFeedbackPercentage,
+}) => (
+  <ul className={s.statList}>
+    <li className={s.statBox}>
+      <p className={s.stat}>Good: {options.good}</p>
+    </li>
 
-export default Statistics
+    <li className={s.statBox}>
+      <p className={s.stat}>Neutral: {options.neutral}</p>
+    </li>
+
+    <li className={s.statBox}>
+      <p className={s.stat}>Bad: {options.bad}</p>
+    </li>
+
+    <li className={s.statBox}>
+      <p className={s.total}>Total: {onCountTotalFeedback}</p>
+    </li>
+
+    <li className={s.statBox}>
+      <p className={s.positivePersentage}>
+        Positive feedback: {onCountPositiveFeedbackPercentage}%
+      </p>
+    </li>
+  </ul>
+);
+
+export default Statistics;
